92dfddb7566793e862fd1ceb0aaa0414bf4f2fa9
Hamza Mahmood <724532@student.inholland.nl>
2026-05-03 19:17:18 +0200

    Accounts
    User can make an account

--- src/main/java/com/banking/controllers/AuthController.java
+++ src/main/java/com/banking/controllers/AuthController.java
@@ -3,10 +3,14 @@ package com.banking.controllers;
 import com.banking.models.dto.request.LoginRequestDTO;
 import com.banking.models.dto.response.LoginResponseDTO;
 import com.banking.services.AuthService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
+
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
