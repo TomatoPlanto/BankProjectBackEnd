fd5e1bdf267daf986b3023431116c9c5b723185f
Thijmenye <729358@student.inholland.nl>
2026-05-04 09:58:24 +0200

    updated the atm controller for the swagger dcs

--- src/main/java/com/banking/controllers/AtmController.java
+++ src/main/java/com/banking/controllers/AtmController.java
@@ -1,6 +1,8 @@
 package com.banking.controllers;
 
 import com.banking.models.dto.request.UpdateBalanceRequestDTO;
+import com.banking.models.dto.response.AccountResponseDTO;
+import com.banking.services.Interface.IAccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,14 +13,18 @@ import com.banking.services.Interface.IAtmService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/atm")
 public class AtmController {
 
     private final IAtmService atmService;
+    private final IAccountService accountService;
 
-    public AtmController(IAtmService atmService) {
+    public AtmController(IAtmService atmService, IAccountService accountService) {
         this.atmService = atmService;
+        this.accountService = accountService;
     }
 
     @PostMapping("/login")
@@ -27,8 +33,26 @@ public class AtmController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/updateBalance")
-    public ResponseEntity<LoginResponseDTO> updateBalance(@Valid @RequestBody UpdateBalanceRequestDTO request) { // need to fix the @ on this one
+    @GetMapping("{accountId}/withdraw")
+    public ResponseEntity<AccountResponseDTO> getWithdrawPage(@PathVariable UUID accountId) {
+        AccountResponseDTO response = accountService.getAccountById(accountId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{accountId}/withdraw")
+    public ResponseEntity<LoginResponseDTO> withdraw(@Valid @RequestBody UpdateBalanceRequestDTO request) { // need to fix the @ on this one
+        LoginResponseDTO response = atmService.updateBalance(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("{accountId}/deposit")
+    public ResponseEntity<AccountResponseDTO> getDepositPage(@PathVariable UUID accountId) {
+        AccountResponseDTO response = accountService.getAccountById(accountId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{accountId}/deposit")
+    public ResponseEntity<LoginResponseDTO> deposit(@Valid @RequestBody UpdateBalanceRequestDTO request) { // need to fix the @ on this one
         LoginResponseDTO response = atmService.updateBalance(request);
         return ResponseEntity.ok(response);
     }
