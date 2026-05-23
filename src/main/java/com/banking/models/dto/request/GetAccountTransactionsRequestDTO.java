862aca23c0805613a5cd3dcf2653861d99a33851
TimSadko <tim.sadko@gmail.com>
2026-05-23 21:43:05 +0200

    Added tests from 3 transactions GET functions.

--- src/main/java/com/banking/models/dto/request/GetAccountTransactionsRequestDTO.java
+++ src/main/java/com/banking/models/dto/request/GetAccountTransactionsRequestDTO.java
@@ -3,6 +3,7 @@ package com.banking.models.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 public class GetAccountTransactionsRequestDTO {
     @NotNull(message = "Account id is required")
     private UUID accountId;
